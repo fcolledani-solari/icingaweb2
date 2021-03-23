e61f738fe9fb6e0fa6a4a2257c12deb6bb25edd6
Yonas Habteab <yonas.habteab@netways.de>
2021-03-23 17:58:18 +0100

    Implement DashboardsController()

---
+++ application/controllers/DashboardsController.php
@@ -0,0 +1,347 @@
+<?php
+
+namespace Icinga\Controllers;
+
+use GuzzleHttp\Psr7\ServerRequest;
+use Icinga\Application\Icinga;
+use Icinga\Exception\Http\HttpNotFoundException;
+use Icinga\Forms\Dashboard\AvailableDashlets;
+use Icinga\Forms\Dashboard\DashletForm;
+use Icinga\Forms\Dashboard\HomeAndPaneForm;
+use Icinga\Web\Widget\Dashboard;
+use Icinga\Web\Widget\Tabextension\DashboardSettings;
+use ipl\Web\Compat\CompatController;
+use ipl\Web\Url;
+
+class DashboardsController extends CompatController
+{
+    /** @var Dashboard */
+    private $dashboard;
+
+    public function init()
+    {
+        $this->dashboard = new Dashboard();
+        $this->dashboard->setUser($this->Auth()->getUser());
+        $this->dashboard->load();
+    }
+
+    public function indexAction()
+    {
+        $homes = $this->dashboard->getHomes();
+        if (array_key_exists(Dashboard::DEFAULT_HOME, $homes)) {
+            $defaultHome = $homes[Dashboard::DEFAULT_HOME];
+            $this->dashboard->loadUserDashboardsFromDatabase($defaultHome->getAttribute('homeId'));
+        }
+
+        $this->createTabs();
+        if (! $this->dashboard->hasPanes()) {
+            $this->setTitle('Dashboard');
+        } else {
+            if (empty($this->dashboard->getPanes())) {
+                $this->setTitle('Dashboard');
+                $this->getTabs()->add('dashboard', array(
+                    'active'    => true,
+                    'title'     => $this->translate('Dashboard'),
+                    'url'       => Url::fromRequest()
+                ))->disableLegacyExtensions();
+            } else {
+                if ($this->getParam('pane')) {
+                    $pane = $this->_getParam('pane');
+                    $this->dashboard->activate($pane);
+                }
+                if ($this->dashboard === null) {
+                    $this->setTitle('Dashboard');
+                } else {
+                    $this->setTitle($this->dashboard->getActivePane()->getTitle());
+                    $this->content->setAttribute('class', 'dashboard content');
+                    $this->addContent($this->dashboard);
+                }
+            }
+        }
+    }
+
+    public function homeAction()
+    {
+        $home = $this->getParam('home');
+
+        if ($home === Dashboard::AVAILABLE_DASHLETS || $home === Dashboard::SHARED_DASHBOARDS) {
+            $this->getTabs()->add($home, [
+                'active'    => true,
+                'label'     => $home,
+                'url'       => Url::fromRequest()
+            ])->disableLegacyExtensions();
+
+            if ($home === Dashboard::AVAILABLE_DASHLETS) {
+                $moduleManager = Icinga::app()->getModuleManager();
+                $dashlets = [];
+
+                foreach ($moduleManager->getLoadedModules() as $module) {
+                    if ($this->dashboard->getUser()->can($moduleManager::MODULE_PERMISSION_NS . $module->getName())) {
+                        if (empty($module->getDashletHomes())) {
+                            continue;
+                        }
+
+                        $dashlets[$module->getName()] = $module->getDashletHomes();
+                    }
+                }
+
+                $this->addContent(new AvailableDashlets($dashlets));
+            }
+        } else {
+            $this->createTabs(true);
+
+            if ($this->getParam('pane')) {
+                $pane = $this->getParam('pane');
+                $this->dashboard->activate($pane);
+            }
+
+            $this->content->setAttribute('class', 'dashboard content');
+            $this->addContent($this->dashboard);
+        }
+    }
+
+    public function renameHomeAction()
+    {
+        $this->getTabs()->add('rename-home', [
+            'active'    => true,
+            'title'     => $this->translate('Update Home'),
+            'url'       => Url::fromRequest()
+        ])->disableLegacyExtensions();
+
+        if (! $this->getParam('home')) {
+            throw new \Zend_Controller_Action_Exception(
+                'Missing parameter "home"',
+                400
+            );
+        }
+
+        $homeForm = new HomeAndPaneForm($this->dashboard);
+        $homeForm->on(HomeAndPaneForm::ON_SUCCESS, function () use ($homeForm) {
+            $this->redirectNow(Url::fromPath('dashboards/settings')->addParams([
+                'home'  => $homeForm->getValue('name')
+            ]));
+        })->handleRequest(ServerRequest::fromGlobals());
+
+        $homes = $this->dashboard->getHomes();
+        $homeForm->load($homes[$this->getParam('home')]);
+
+        $this->addContent($homeForm);
+    }
+
+    public function removeHomeAction()
+    {
+        $this->getTabs()->add('remove-home', array(
+            'active'    => true,
+            'label'     => $this->translate('Remove Home'),
+            'url'       => Url::fromRequest()
+        ))->disableLegacyExtensions();
+
+        if (! $this->getParam('home')) {
+            throw new \Zend_Controller_Action_Exception(
+                'Missing parameter "home"',
+                400
+            );
+        }
+
+        $homeForm = (new HomeAndPaneForm($this->dashboard))
+            ->setAction((string)Url::fromRequest())
+            ->on(HomeAndPaneForm::ON_SUCCESS, function () {
+                // Since the navigation menu is not loaded that fast, we need to unset
+                // the just deleted home from this array as well.
+                unset($this->dashboard->getHomes()[$this->getParam('home')]);
+
+                $firstHome = $this->dashboard->rewindHomes();
+                if (empty($firstHome)) {
+                    $this->redirectNow('dashboard');
+                } else {
+                    $this->redirectNow(Url::fromPath('dashboard/settings')->addParams([
+                        'home'  => $firstHome->getName()
+                    ]));
+                }
+            })
+            ->handleRequest(ServerRequest::fromGlobals());
+
+        $homeForm->load($this->dashboard->getHomes()[$this->getParam('home')]);
+        $this->addContent($homeForm);
+    }
+
+    public function renamePaneAction()
+    {
+        $this->getTabs()->add('update-pane', [
+            'active'    => true,
+            'title'     => $this->translate('Update Pane'),
+            'url'       => Url::fromRequest()
+        ])->disableLegacyExtensions();
+
+        $home = $this->getParam('home');
+        if (! array_key_exists($home, $this->dashboard->getHomes())) {
+            throw new HttpNotFoundException('Home not found');
+        }
+
+        $pane = $this->getParam('pane');
+        if (! $this->dashboard->hasPane($pane)) {
+            throw new HttpNotFoundException('Pane not found');
+        }
+
+        $paneForm = (new HomeAndPaneForm($this->dashboard))
+            ->on(HomeAndPaneForm::ON_SUCCESS, function () {
+                $this->redirectNow('__back__');
+            })
+            ->handleRequest(ServerRequest::fromGlobals());
+
+        $paneForm->load($this->dashboard->getPane($pane));
+        $this->addContent($paneForm);
+    }
+
+    public function removePaneAction()
+    {
+        $this->getTabs()->add('remove-pane', array(
+            'active'    => true,
+            'label'     => $this->translate('Remove Pane'),
+            'url'       => Url::fromRequest()
+        ))->disableLegacyExtensions();
+
+        $home = $this->getParam('home');
+        if (! array_key_exists($home, $this->dashboard->getHomes())) {
+            throw new HttpNotFoundException('Home not found');
+        }
+
+        $pane = $this->getParam('pane');
+        if (! $this->dashboard->hasPane($pane)) {
+            throw new HttpNotFoundException('Pane not found');
+        }
+
+        $paneForm = (new HomeAndPaneForm($this->dashboard))
+            ->on(HomeAndPaneForm::ON_SUCCESS, function () use ($home) {
+                $this->redirectNow(Url::fromPath('dashboards/settings')->addParams([
+                    'home'  => $home
+                ]));
+            })
+            ->handleRequest(ServerRequest::fromGlobals());
+
+        $paneForm->load($this->dashboard->getPane($pane));
+        $this->addContent($paneForm);
+    }
+
+    public function newDashletAction()
+    {
+        $this->getTabs()->add('new-dashlet', array(
+            'active'    => true,
+            'label'     => $this->translate('New Dashlet'),
+            'url'       => Url::fromRequest()
+        ))->disableLegacyExtensions();
+
+        $dashletForm = new DashletForm($this->dashboard);
+        $dashletForm->on(DashletForm::ON_SUCCESS, function () use ($dashletForm) {
+            $this->redirectNow(Url::fromPath('dashboards/home')->addParams([
+                'home'  => $dashletForm->getValue('home'),
+                'pane'  => $dashletForm->getValue('pane'),
+            ]));
+        })->handleRequest(ServerRequest::fromGlobals());
+
+        if ($this->getParam('url')) {
+            $params = $this->getAllParams();
+            $params['url'] = rawurldecode($this->getParam('url'));
+            $dashletForm->populate($params);
+        }
+
+        $this->addContent($dashletForm);
+    }
+
+    public function updateDashletAction()
+    {
+        $this->getTabs()->add('update-dashlet', array(
+            'active'    => true,
+            'label'     => $this->translate('Update Dashlet'),
+            'url'       => Url::fromRequest()
+        ))->disableLegacyExtensions();
+
+        $this->isMissingSomething();
+
+        $pane = $this->dashboard->getPane($this->getParam('pane'));
+        $dashlet = $pane->getDashlet($this->getParam('dashlet'));
+
+        $dashletForm = new DashletForm($this->dashboard);
+        $dashletForm->on(DashletForm::ON_SUCCESS, function () use ($dashletForm) {
+            $this->redirectNow(Url::fromPath('dashboards/settings')->addParams([
+                'home'  => $dashletForm->getValue('home')
+            ]));
+        })->handleRequest(ServerRequest::fromGlobals());
+
+        $dashletForm->load($dashlet);
+        $this->addContent($dashletForm);
+    }
+
+    public function removeDashletAction()
+    {
+        $this->getTabs()->add('remove-dashlet', array(
+            'active'    => true,
+            'label'     => $this->translate('Remove Dashlet'),
+            'url'       => Url::fromRequest()
+        ))->disableLegacyExtensions();
+
+        $this->isMissingSomething();
+        $dashletForm = (new DashletForm($this->dashboard))
+            ->on(DashletForm::ON_SUCCESS, function () {
+                $this->redirectNow(Url::fromPath('dashboards/settings')->addParams([
+                    'home'  => $this->getParam('home')
+                ]));
+            })
+            ->handleRequest(ServerRequest::fromGlobals());
+
+        $this->addContent($dashletForm);
+    }
+
+    public function settingsAction()
+    {
+        $this->createTabs();
+        $controlForm = new Dashboard\SettingSortBox($this->dashboard);
+        $controlForm->on(Dashboard\SettingSortBox::ON_SUCCESS, function () use ($controlForm) {
+            $this->redirectNow(Url::fromPath('dashboard/settings')->addParams([
+                'home' => $controlForm->getPopulatedValue('sort_dashboard_home')
+            ]));
+        })->handleRequest(ServerRequest::fromGlobals());
+
+        $this->addControl($controlForm);
+        $this->addContent(new Dashboard\Settings($this->dashboard));
+    }
+
+    private function createTabs($defaultPanes = false)
+    {
+        if ($this->getParam('home')) {
+            $urlParam = ['home' => $this->getParam('home')];
+        } else {
+            $home = $this->dashboard->rewindHomes();
+            $urlParam = ['home' => !$home ?: $home->getName()];
+        }
+
+        $this->controls->setTabs($this->dashboard->getTabs($defaultPanes)->extend(new DashboardSettings($urlParam)));
+    }
+
+    /**
+     * Check if any of the required parameters are missing
+     *
+     * @throws \Zend_Controller_Action_Exception
+     */
+    private function isMissingSomething()
+    {
+        if (! $this->getParam('home')) {
+            throw new \Zend_Controller_Action_Exception(
+                'Missing parameter "home"',
+                400
+            );
+        }
+        if (! $this->getParam('pane')) {
+            throw new \Zend_Controller_Action_Exception(
+                'Missing parameter "pane"',
+                400
+            );
+        }
+        if (! $this->getParam('dashlet')) {
+            throw new \Zend_Controller_Action_Exception(
+                'Missing parameter "dashlet"',
+                400
+            );
+        }
+    }
+}
